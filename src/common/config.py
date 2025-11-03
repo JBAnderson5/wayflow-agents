20a8605361c5eb0779cb7f033bf4cf5d17309fe5
Kaushik Kundu <kaushik.kundu@oracle.com>
2025-11-03 19:35:08 +0530

    Update config.py

--- src/common/config.py
+++ src/common/config.py
@@ -10,20 +10,23 @@ THIS_DIR     = Path(__file__).resolve()
 PROJECT_ROOT = THIS_DIR.parent.parent.parent
 load_dotenv(PROJECT_ROOT  / "config/env") # expects OCI_ vars in env
 
+#────────────────────────────────────────────────────────
+# AIDP JDBC Connectivity
+# ───────────────────────────────────────────────────────
 OCI_CONFIG_FILE       	= os.getenv("OCI_CONFIG_FILE")
-JDBC_AUTH_TYPE = os.getenv("AUTH_TYPE")
+JDBC_AUTH_TYPE 			= os.getenv("AUTH_TYPE")
 JAVA_HOME       		= os.getenv("JAVA_HOME")
 JDBC_DRIVER_CLASS_NAME  = os.getenv("JDBC_DRIVER_CLASS_NAME")
 JDBC_URL       			= os.getenv("JDBC_URL")
 
 #────────────────────────────────────────────────────────
 # OCI Security configuration
-# ────────────────────────────────────────────────────────
+# ───────────────────────────────────────────────────────
 AUTH_TYPE = os.getenv("AUTH_TYPE")
 CONFIG_PROFILE = os.getenv("CONFIG_PROFILE")
 #────────────────────────────────────────────────────────
 # OCI GenAI configuration
-# ────────────────────────────────────────────────────────
+# ───────────────────────────────────────────────────────
 COMPARTMENT_ID 	 	  	= os.getenv("OCI_COMPARTMENT_ID")
 ENDPOINT       			= os.getenv("OCI_GENAI_ENDPOINT")
 MODEL_ID       			= os.getenv("OCI_GENAI_MODEL_ID")
